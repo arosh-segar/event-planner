f88db3870edb1ef9762d2d21d0383f7b6a0f96d8
arosh-segar <arosh.segar@gmail.com>
2021-09-20 11:32:59 +0530

    Event list and add task

--- components/Event/Event.js
+++ components/Event/Event.js
@@ -1,44 +1,34 @@
 import React from 'react';
 
 import Icon from 'react-native-vector-icons/Entypo';
-import {  Text, NativeBaseProvider, Box, useMediaQuery} from "native-base";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
-function Guest(){
-
-
-  return(
+import {
+  Text,
+  NativeBaseProvider,
+  Box,
+  VStack,
+  useMediaQuery,
+  Center,
+} from 'native-base';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEllipsisV} from '@fortawesome/free-solid-svg-icons';
+function Guest() {
+  return (
     <NativeBaseProvider>
-
-      <Box
-        bg="white"
-        shadow={2}
-        rounded="lg"
-        minWidth="80%"
-        h={20}
-        mb={5}
-        ml={5}
-        bg="#0284c7"
-
-      >
-
-
-        <Text color="#FFFFFF"  mb={3} mt={3} ml={3} >
-          Event
-        </Text>
-        <Text color="#FFFFFF" ml={3}>
-          Venue
-        </Text>
-        <FontAwesomeIcon icon={faEllipsisV} color={"white"} style={{left:350,bottom:60}}/>
-
-      </Box>
-
+      <Center>
+        <Box bg="#0284c7" shadow={2} rounded="lg" w="90%" h={20} mb={5}>
+          <Text color="#FFFFFF" mb={3} mt={3} ml={3}>
+            Event
+          </Text>
+          <Text color="#FFFFFF" ml={3}>
+            Venue
+          </Text>
+          <VStack position="absolute" right="5%" top="40%">
+            <FontAwesomeIcon icon={faEllipsisV} color={'white'} />
+          </VStack>
+        </Box>
+      </Center>
     </NativeBaseProvider>
-
-  )
-
+  );
 }
 
-
-
-export default Guest
+export default Guest;
--- components/Event/Events.js
+++ components/Event/Events.js
@@ -1,94 +1,63 @@
 import React from 'react';
-import {StyleSheet, View, Text, ScrollView,Alert} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, Alert} from 'react-native';
+import {NativeBaseProvider, Center} from 'native-base';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import MaterialCommunityIconsIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Event from './Event';
-import { FloatingAction } from "react-native-floating-action";
+import {FloatingAction} from 'react-native-floating-action';
+import {style} from 'styled-system';
 
-
-
-class Events extends React.Component{
-
-
+class Events extends React.Component {
   render() {
-
-    const {navigation} = this.props
+    const {navigation} = this.props;
 
     return (
       <View style={styles.container}>
         {/*<EntypoIcon name="arrow-long-left" style={styles.icon}></EntypoIcon>*/}
 
-            <View style={styles.scrollArea1}>
-                <ScrollView>
-                  <Event/>
-                  <Event/>
-                  <Event/>
-                  <Event/>
-                  <Event/>
-                  <Event/>
-                  <Event/>
-                  <Event/>
-                  <Event/>
-                </ScrollView>
-              <FloatingAction
-                showBackground={false}
-                animated={false}
-                onPressMain={()=>{
-                   navigation.navigate("AddEvent")
-                }}/>
-            </View>
+        <View style={styles.scrollArea1}>
+          <ScrollView style={styles.scrollArea2}>
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+          </ScrollView>
+          <FloatingAction
+            showBackground={false}
+            animated={false}
+            onPressMain={() => {
+              navigation.navigate('AddEvent');
+            }}
+          />
+        </View>
       </View>
-    )
+    );
   }
-
-
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    height: '100%',
   },
   icon: {
-    color: "rgba(128,128,128,1)",
+    color: 'rgba(128,128,128,1)',
     fontSize: 40,
     marginTop: 75,
-    marginLeft: 19
-  },
-  rect: {
-    top: 0,
-    width: 375,
-    height: 84,
-    position: "absolute",
-    backgroundColor: "#E6E6E6",
-    left: 0
+    marginLeft: 19,
   },
   scrollArea1: {
-    top: 81,
-    width: 405,
-    height: 404,
-    position: "absolute",
-    left: 0
+    width: '100%',
+    height: '100%',
   },
-  scrollArea1_contentContainerStyle: {
-    height: 84,
-    width: 375
+  scrollArea2: {
+    width: '100%',
   },
-  rectStack: {
-    width: 375,
-    height: 165
-  },
-  icon2: {
-    color: "rgba(128,128,128,1)",
-    fontSize: 40,
-    marginLeft: 75,
-    marginTop: 143
-  },
-  rectStackRow: {
-    height: 186,
-    flexDirection: "row",
-    marginTop: 7,
-    marginRight: -115
-  }
 });
 
-export default Events
+export default Events;
