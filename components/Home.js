1e193240b1bf60f2f16e85aede8c02823da2367a
Sabesan <sabesanganeshalingam@gmail.com>
2021-09-20 11:05:55 +0530

    Home Page

--- components/Home.js
+++ components/Home.js
@@ -1,114 +1,58 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+
 import {heightPercentageToDP as hp,widthPercentageToDP as wp} from 'react-native-responsive-screen'
 import Events from './Event/Events';
+import {Image, Button, Center, NativeBaseProvider,Text } from "native-base"
 
 
 
 function Home({navigation}) {
   return (
-    <View style={styles.container}>
-      <View style={styles.rectRow}>
-        <View style={styles.rect} onTouchStart={()=>{
-          navigation.navigate("Events")
-        }}>
-          <Text style={styles.events}>Events</Text>
-        </View>
-        <View style={styles.rect2} onTouchStart={()=>{
-          navigation.navigate("Guests")
-        }}>
-          <Text style={styles.guests}>Guests</Text>
-        </View>
-      </View>
-      <View style={styles.rect3Row}>
-        <View style={styles.rect3}  onTouchStart={()=>{
-          navigation.navigate("To Do List")
-        }}>
-          <Text style={styles.toDoList}>To-Do-List</Text>
-        </View>
-        <View style={styles.rect4}  onTouchStart={()=>{
-          navigation.navigate("ShoppingList")
-        }}>
-          <Text style={styles.shoppingList}>Shopping List</Text>
-        </View>
-      </View>
-    </View>
-  )
-};
+      <NativeBaseProvider>
+        <Center flex={1}>
+      <Button   onPress={() => navigation.navigate("Events")}>
+        <Image
+            source={{
+              uri: "https://wallpaperaccess.com/full/317501.jpg",
+            }}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  rect: {
-    width: wp('35%'),
-    height: hp('18%'),
-    backgroundColor: "#E6E6E6"
-  },
-  events: {
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    fontSize: hp('3%'),
-    marginTop: hp('6%'),
-    marginLeft: wp('10%')
-  },
-  rect2: {
-    width: wp('35%'),
-    height: hp('18%'),
-    backgroundColor: "#E6E6E6",
-    marginLeft: wp('4%')
-  },
-  guests: {
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    fontSize: hp('3%'),
-    marginTop: hp('6%'),
-    marginLeft: wp('10%')
-  },
-  rectRow: {
-    height: hp('1%'),
-    flexDirection: "row",
-    marginTop: hp('30%'),
-    marginLeft: wp('14%')
-  },
-  rect3: {
-    width: wp('35%'),
-    height: hp('18%'),
-    backgroundColor: "#E6E6E6"
-  },
-  toDoList: {
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    fontSize: hp('3%'),
-    marginTop: hp('6%'),
-    marginLeft: wp('8%')
-  },
-  rect4: {
-    width: wp('35%'),
-    height: hp('18%'),
-    backgroundColor: "#E6E6E6",
-    marginLeft: wp('4%')
-  },
-  shoppingList: {
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    fontSize: hp('3%'),
-    marginTop: hp('6%'),
-    marginLeft: wp('8%')
-  },
-  rect3Row: {
-    height: hp('1%'),
-    flexDirection: "row",
-    marginTop: hp('20%'),
-    marginLeft: wp('14%')
-  }
-});
+            alt="Alternate Text"
+            size={"lg"}
+        />
+        <Text>Events</Text></Button>
+            <Button   onPress={() => navigation.navigate("Events")}>
+                <Image
+                    source={{
+                        uri: "https://wallpaperaccess.com/full/317501.jpg",
+                    }}
+
+                    alt="Alternate Text"
+                    size={"lg"}
+                />
+                <Text>Guests</Text></Button>
+            <Button   onPress={() => navigation.navigate("Events")}>
+            <Image
+                source={{
+                    uri: "https://wallpaperaccess.com/full/317501.jpg",
+                }}
+
+                alt="Alternate Text"
+                size={"lg"}
+            />
+            <Text>TO-Do List</Text></Button>
+            <Button   onPress={() => navigation.navigate("Events")}>
+                <Image
+                    source={{
+                        uri: "https://wallpaperaccess.com/full/317501.jpg",
+                    }}
+
+                    alt="Alternate Text"
+                    size={"lg"}
+                />
+                <Text>SHOPPING LIST</Text></Button>
+
+            </Center>
+              </NativeBaseProvider>
+
+)};
 export default Home;
